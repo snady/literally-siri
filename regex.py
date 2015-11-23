82399a83e3bbea1235def2bbb8ffb582f2ab9665
Jion Fairchild <jijiglobe@yahoo.com>
2015-11-23 17:54:44 -0500

    added mandatory spaces to names reg:negative_squared_cross_mark:

--- regex.py
+++ regex.py
@@ -1,3 +1,4 @@
-name = "/[A-Z][a-z]+ ?[A-Z][a-z]+/g"
+#Regular expressions that will find the following
+name = "/[A-Z][a-z]+ [A-Z][a-z]+/g"
 numbers = "/\(?[0-9]{3}\)? ?-? ?[0-9]{3} ?-? ?[0-9]{4}/g"
 
