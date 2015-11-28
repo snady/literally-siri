895748bc54993ca5067ad085c7dd03b2bbcc2966
boying44 <boying.tang.44@gmail.com>
2015-11-27 22:21:19 -0500

    fixed jion's regex .-.

--- util.py
+++ util.py
@@ -27,28 +27,32 @@ def strip(q):
             page = u.read()
             soup = bs4.BeautifulSoup(page,'html')
             raw = soup.get_text()
+           
             unicodeVer = re.sub("[\t\n ]"," ",raw)
             unicodeVer.encode('utf8')
-            str(unicodeVer)
-            print type(unicodeVer)
-            unicodeVer+= "123-456-7890"
-            text.append(unicodeVer)
+            reg = str(unicodeVer)
+#            print type(reg)
+#            reg += "123-456-7890"
+            
+            text.append(reg)
         except:
             pass
     return text
 
 
 def findNumbers(text):
-    print  re.findall(regex.number,text)
+    return re.findall(regex.number,text)
     
 def getNumQuery(query):
     ans = []
     vals = strip(query)
     for x in vals:
-        print type(x)
+#        print type(x)
 #        time.sleep(1)
         ans+=findNumbers(x)
     return ans
+
+#print re.findall(regex.number,"123-456-7890")
 
 while True:
     print("Give me a query")
