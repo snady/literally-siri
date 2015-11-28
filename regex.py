5250effa53f85fd089df38a80408eaaae6a40b51
Jion Fairchild <jijiglobe@yahoo.com>
2015-11-27 20:11:51 -0500

    added date regex

--- regex.py
+++ regex.py
@@ -1,4 +1,4 @@
 #Regular expressions that will find the following
 name = "/[A-Z][a-z]+ [A-Z][a-z]+/g"
 numbers = "/\(?[0-9]{3}\)? ?-? ?[0-9]{3} ?-? ?[0-9]{4}/g"
-
+date = "/[10]?[0-9][\/-][0-3]?[0-9][\/-]([12][0-9])?[0-9]{2}/g"
